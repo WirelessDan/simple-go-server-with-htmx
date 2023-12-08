536f21552b947d7acfc0d0ec60d0919b23740b3c
Wireless Dan <wirelessdan@proton.me>
2023-12-07 19:58:24 -0500

    Simple test page that returns a string and request

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("hello world")
+
+	// define a handler function
+	// the first argument is the URL, '/'
+	// the second is h1, a handler defined above the function
+
+	h1 := func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "hello world\n")
+		io.WriteString(w, r.Method)
+	}
+	http.HandleFunc("/", h1)
+
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
